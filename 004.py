2b46b67d1c40723317c6325b3c8d0a101d1d0507
tsutsumi-ozro <soccerbakadaiki@iclud.com>
2022-11-08 14:44:38 +0900

    commit

--- 004.py
+++ 004.py
@@ -7,16 +7,16 @@ for h in range(H):
 
 new_grid = [[0]*W for i in range(H)]
 
+#合計を追加していく
 for h in range(H):
     sum_A = sum(A[h])
     for w in range(W):
         new_grid[h][w] += sum_A
-
-
 for w in range(W):
     sum_A_T = sum(A_T[w])
     for h in range(H):
         new_grid[h][w] += sum_A_T
+#自分自身は二回足されているので引いていく
 for h in range(H):
     for w in range(W):
         new_grid[h][w] -= A[h][w]
